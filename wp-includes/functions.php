80b810d5df818a629fa66a5ba07a7b56dcd9f19d
gitkyo <p.delaunay1@gmail.com>
2017-07-18 16:52:28 +0200

    add and edit htaccess for improve security

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -5642,3 +5642,12 @@ function wp_cache_get_last_changed( $group ) {
 
 	return $last_changed;
 }
+
+add_action(‘template_redirect’, ‘bwp_template_redirect’);
+function bwp_template_redirect()
+{
+if (is_author())
+{
+wp_redirect( home_url() ); exit;
+}
+}
